d780af7c869e29656ed090b24e5154a661795efb
Karina Chen <karina6188@gmail.com>
2019-10-23 11:17:33 -0700

    add UserInterface method

--- lab07-collections/lab07-collections/Book.cs
+++ lab07-collections/lab07-collections/Book.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace lab07_collections
-{ 
+{
     class Book
     {
         public Book Title { get; set; }
+
+        public enum Genre {get; set;}
     }
 }
--- lab07-collections/lab07-collections/Program.cs
+++ lab07-collections/lab07-collections/Program.cs
@@ -10,6 +10,68 @@ namespace lab07_collections
             Library<Book> Library = new Library<Book>();
             List<Book> BookBag = new List<Book>();
 
+            bool menu = true;
+            while (menu)
+            {
+                menu = UserInterface();
+            }
+        }
+
+        static bool UserInterface()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to Hogwart's Mystery Library");
+            Console.WriteLine("The books here are only available to wizards not to muggles!");
+            Console.WriteLine("Please select one of the options from the menu.");
+            Console.WriteLine("1) View all books");
+            Console.WriteLine("2) Add a book");
+            Console.WriteLine("3) Borrow a book");
+            Console.WriteLine("4) Return a book");
+            Console.WriteLine("5) View book bag");
+            Console.WriteLine("6) Exit");
+            string selection = Console.ReadLine();
+            try
+            {
+                switch (selection)
+                {
+                    case "1":
+                        ViewAllBooks();
+                        Console.ReadLine();
+                        return true;
+
+                    case "2":
+                        AddBooks();
+                        Console.ReadLine();
+                        return true;
+
+                    case "3":
+                        BorrowBooks();
+                        return true;
+
+                    case "4":
+                        ReturnBooks();
+                        return true;
+
+                    case "5":
+                        ViewBookBag();
+                        Console.ReadLine();
+                        return true;
+
+                    case "6":
+                        Console.WriteLine("Come back next time to learn more wizard tricks.");
+                        Console.ReadLine();
+                        return false;
+
+                    default:
+                        Console.WriteLine("Your selection is invalid.");
+                        Console.ReadLine();
+                        return true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
