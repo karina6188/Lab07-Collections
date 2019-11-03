00be390aa86c24047b1d8c39a2d69b797a72e7ef
Karina Chen <karina6188@gmail.com>
2019-11-02 20:59:42 -0700

    add unit testing

---
+++ lab07-collections/CollectionsTests/UnitTest1.cs
@@ -0,0 +1,44 @@
+using System;
+using Xunit;
+
+namespace CollectionsTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void AddBook()
+        {
+
+        }
+
+        [Fact]
+        public void RemoveBook()
+        {
+
+        }
+
+        [Fact]
+        public void RemoveNone()
+        {
+
+        }
+
+        [Fact]
+        public void GetSetBook()
+        {
+
+        }
+
+        [Fact]
+        public void GetSetAuthor()
+        {
+
+        }
+
+        [Fact]
+        public void CountBooks()
+        {
+
+        }
+    }
+}
